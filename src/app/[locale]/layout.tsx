945431aeb52fdee1fbee18b16ed9a5e4ddcbd454
Martin <mailmartinmendez@gmail.com>
2026-05-28 01:03:03 -0300

    Add generateStaticParams to LocaleLayout for build-time static page generation of locales

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -8,6 +8,10 @@ type Props = {
   params: Promise<{ locale: string }>;
 };
 
+export function generateStaticParams() {
+  return routing.locales.map((locale) => ({ locale }));
+}
+
 export default async function LocaleLayout({ children, params }: Props) {
   const { locale } = await params;
 
